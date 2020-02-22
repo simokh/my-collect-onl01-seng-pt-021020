b662b93209eb58d93831bdbcbe2876cccf4d3054
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 00:43:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,20 @@
-# def my_collect(language)
-#   collection= []
-#   i = 0
-#   while i < language.length
-#     collection << (language[i])
-#     i += 1
-#   end
-#   my_collect(array) {|i| i.split(" ").first}
-# end
-# # my_collect(array) {|i| i.split(" ").first}
-def my_collect(array)
+def my_collect(language)
+  collection= []
   i = 0
-  name_collection = []
-  while i < array.length
-    name_collection.push yield(array[i])
-  # you could also do it this way:
-  # name_collection << yield(array[i])
+  while i < language.length
+    collection << yield(language[i])
     i += 1
   end
-  name_collection
 end
+
+# def my_collect(array)
+#   i = 0
+#   name_collection = []
+#   while i < array.length
+#     name_collection.push yield(array[i])
+#   # you could also do it this way:
+#   # name_collection << yield(array[i])
+#     i += 1
+#   end
+#   name_collection
+# end
