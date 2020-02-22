03da3880dc463acc946233713e7e043cc82b2400
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 00:43:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -12,8 +12,6 @@ end
 #   name_collection = []
 #   while i < array.length
 #     name_collection.push yield(array[i])
-#   # you could also do it this way:
-#   # name_collection << yield(array[i])
 #     i += 1
 #   end
 #   name_collection
